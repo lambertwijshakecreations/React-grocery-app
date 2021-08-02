490e55be9c1a9251c192d31beebb7634f5e4f25d
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-02 09:41:23 +0200

    Made a list With 5 items

---
+++ src/components/list.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class List extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<ul>
+				<li>boter</li>
+				<li>sla</li>
+				<li>melk</li>
+				<li>paprika</li>
+				<li>bakpapier</li>
+			</ul>
+		);
+	}
+}
+
+export default List;
