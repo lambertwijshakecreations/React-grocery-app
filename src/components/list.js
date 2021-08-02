c10457f4adce512816c42d04f614d05a57ff1df8
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-02 12:00:24 +0200

    redid Lesson1

--- src/components/list.js
+++ src/components/list.js
@@ -1,43 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import ListItem from "./listItem";
 
-class List extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			groceryItems: [
-				{ id: 1, title: "boter" },
-				{ id: 2, title: "melk" },
-				{ id: 3, title: "sla" },
-				{ id: 4, title: "paprika" },
-				{ id: 5, title: "bakpapier" }
-			]
-		};
+class List extends Component {
+	constructor() {
+		super();
+		this.state = {};
 	}
 
 	render() {
-		const listItem = groceryItems.map((item) => {
-			item.title;
-		});
-
 		return (
-			<ul>
-				<li>
-					<ListItem title="boter" />
-				</li>
-				<li>
-					<ListItem title="melk" />
-				</li>
-				<li>
-					<ListItem title="sla" />
-				</li>
-				<li>
-					<ListItem title="paprika" />
-				</li>
-				<li>
-					<ListItem title="bakpapier" />
-				</li>
-			</ul>
+			<div>
+				<ul>
+					<ListItem />
+				</ul>
+			</div>
 		);
 	}
 }
