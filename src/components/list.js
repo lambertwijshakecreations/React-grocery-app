7b8b990b81ff2d69dab68039ee62a43c5699e767
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-02 10:21:51 +0200

    Added State items

--- src/components/list.js
+++ src/components/list.js
@@ -1,19 +1,38 @@
 import React from "react";
+import ListItem from "./listItem";
 
 class List extends React.Component {
-	constructor() {
-		super();
-		this.state = {};
+	constructor(props) {
+		super(props);
+		this.state = {
+			groceryItems: [
+				{ id: 1, title: "boter" },
+				{ id: 2, title: "melk" },
+				{ id: 3, title: "sla" },
+				{ id: 4, title: "paprika" },
+				{ id: 5, title: "bakpapier" }
+			]
+		};
 	}
 
 	render() {
 		return (
 			<ul>
-				<li>boter</li>
-				<li>sla</li>
-				<li>melk</li>
-				<li>paprika</li>
-				<li>bakpapier</li>
+				<li>
+					<ListItem title="boter" />
+				</li>
+				<li>
+					<ListItem title="melk" />
+				</li>
+				<li>
+					<ListItem title="sla" />
+				</li>
+				<li>
+					<ListItem title="paprika" />
+				</li>
+				<li>
+					<ListItem title="bakpapier" />
+				</li>
 			</ul>
 		);
 	}
