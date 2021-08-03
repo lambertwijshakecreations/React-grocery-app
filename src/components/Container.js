2edb70648dfff41a937b221b32b82a6ed92664f9
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-03 11:27:27 +0200

    turned Contaner function into a Class and added a state

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import ShoppingCart from "./ShoppingCart";
 import GroceryList from "./GroceryList";
 
-const Container = () => {
-	return (
-		<div>
-			<GroceryList />
-			<ShoppingCart />
-		</div>
-	);
-};
+class Container extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			groceryItems: [
+				{ id: 1, title: "melk" },
+				{ id: 2, title: "boter" },
+				{ id: 3, title: "bier" },
+				{ id: 4, title: "sla melange" },
+				{ id: 5, title: "chips" }
+			]
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<GroceryList />
+				<ShoppingCart />
+			</div>
+		);
+	}
+}
 
 export default Container;
