395a5ada61e5f0221c68c2e96915f426095085ab
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-03 11:28:31 +0200

    removed state from list to container

--- src/components/list.js
+++ src/components/list.js
@@ -4,22 +4,13 @@ import ListItem from "./listItem";
 class List extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			groceryItems: [
-				{ id: 1, title: "melk" },
-				{ id: 2, title: "boter" },
-				{ id: 3, title: "bier" },
-				{ id: 4, title: "sla melange" },
-				{ id: 5, title: "chips" }
-			]
-		};
+		this.state = {};
 	}
 
 	render() {
 		const listItems = this.state.groceryItems.map((item) => (
 			<ListItem key={item.id} title={item.title} />
 		));
-		console.log(listItems);
 
 		return (
 			<div>
