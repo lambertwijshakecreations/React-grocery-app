35d3bad20324a1e2756bae2d3bdcb15df0a43055
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-05 10:19:26 +0200

    made Container Function into a Class

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ShoppingCart from "./ShoppingCart";
 import GroceryList from "./GroceryList";
 
-const Container = () => {
-	return (
-		<div>
-			<h1>Container</h1>
-			<ShoppingCart />
-			<GroceryList />
-		</div>
-	);
-};
+class Container extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Container</h1>
+				<ShoppingCart />
+				<GroceryList />
+			</div>
+		);
+	}
+}
 
 export default Container;
