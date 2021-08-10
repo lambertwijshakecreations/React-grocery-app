3b5943d7cb3de041a9ebc504168926eea95f34b1
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-08-10 10:23:56 +0200

    added a simple input, combined with GroceryList

---
+++ src/components/inputField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputField = () => {
+	return (
+		<form className="groceries-input">
+			<input placeholder="Voeg een object toe"></input>
+		</form>
+	);
+};
+
+export default InputField;
--- src/containers/GroceryList.js
+++ src/containers/GroceryList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import List from "../components/List/List";
+import InputField from "../components/inputField";
 
 const GroceryList = ({ groceryItems, handleClickGroceryItem }) => {
 	return (
 		<div>
+			<InputField />
 			<List items={groceryItems} handleClickItem={handleClickGroceryItem} />
 		</div>
 	);
